365cb357b810e270d7db4ff1f39fb7a513bc88da
Toothtoothtooth <94367162+Toothtoothtooth@users.noreply.github.com>
2021-11-15 16:40:50 +0300

    Add files via upload

---
+++ serv mash.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks
+
+namespace serv 
+{
+    class people
+    {
+        public string name;
+        public string type;
+        public int request;
+        public string preLaunchTask;
+      public int age;
+        cin>>
+    }
+    class Program
+    {
+
+        static people GetPeople()
+        {
+        public.string name;
+        public.string type;
+        public.int request;
+        public.string preLaunchTask;
+        public.int age =;
+         return people
+    }
+    }
+        static void Print(people people)
+{
+
+    cout << (people.name);
+    cout << (people.type);
+    cout << (people.request);
+    cout << (people.preLaunchTask); 
+    cout << (people.age);
+}
+        static void Main(string[] args)
+        {
+        cin>>name;
+        cin >> type;
+        cin >> request;
+        cin >> preLaunchTask;
+        cin >> age;
+
+    var people = GetPeople();
+
+    Print(people);
+    }
+}
+
